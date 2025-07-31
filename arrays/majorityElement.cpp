b5d4f2ec0fd1deed6e4bd4e5a524010435c6ddbb
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-31 21:47:06 +0530

    find majority element in array in O(n) time with O(1) space

--- arrays/majorityElement.cpp
+++ arrays/majorityElement.cpp
@@ -3,6 +3,30 @@
 #include <map>
 using namespace std;
 
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     vector<int> nums(n);
+
+//     for (int i=0; i<n; i++) {
+//         cin >> nums[i];
+//     }
+
+//     map<int, int> mpp;
+
+//     for (int i=0; i<n; i++) {
+//         mpp[nums[i]]++;
+//     }
+
+//     for (auto it : mpp) {
+//         if (it.second > (n/2)) {
+//             cout << it.first << " Number of times it appear: " << it.second << endl;
+//         }
+//     }
+// }
+
+
 int main() {
     int n;
     cin >> n;
@@ -13,15 +37,21 @@ int main() {
         cin >> nums[i];
     }
 
-    map<int, int> mpp;
+    int count = 0;
+    int ele;
 
     for (int i=0; i<n; i++) {
-        mpp[nums[i]]++;
+        if (count == 0) {
+            count = 1;
+            ele = nums[i];
+        } else if (nums[i] == ele) count++;
+        else count--;
     }
 
-    for (auto it : mpp) {
-        if (it.second > (n/2)) {
-            cout << it.first << " Number of times it appear: " << it.second << endl;
-        }
+    int majorityEle = 0;
+    for (int i=0; i<n; i++) {
+        if (nums[i] == ele) majorityEle++;
     }
+
+    cout << "Number of times it appear: " << majorityEle << " Element: " << ele << endl;
 }
